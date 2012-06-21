ec6db0477331f74044e94a79206ad93609afa1e3
Rafael Caricio <rafael.jacinto@gmail.com>
2012-06-21 01:12:16 -0300

    Primeiro protótipo do plugin.

--- js/application.js
+++ js/application.js
@@ -1,9 +1,21 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-32823945-1']);
-//_gaq.push(['_trackPageview']);
+_gaq.push(['_trackPageview']);
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = 'https://ssl.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 
+(function($) {
+
+    $.ajax("http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp", {
+        complete: function(req) {
+            var data = req.responseText.split('|');
+            $('#numero-concurso').text(data[0]);
+            $('#data-sorteio').text(data[11]);
+            $('#resultado').html(data[20]);
+        }
+    });
+
+})(jQuery);
